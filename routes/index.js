0d4aaeee5ffbbe95149c46e5b57466158f24a878
Rohit Sharma <rohit7209@rediffmail.com>
2017-12-03 23:11:07 +0530

    html-pdf work started

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/asd', function(req, res, next) {
+  res.render('index', { title: 'Express 121' });
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const PDFDocument = require('pdfkit')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/test', function(req, res, next) {
+  //res.send('respond with a resource');
+  res.json([{
+    id:1,
+    name:"rohit"
+  },
+  {
+    id:2,
+    name:"mohit"
+  }]);
 });
+
+
+router.get('/pdf', (req, res, next) => {
+  const doc = new PDFDocument()
+  let filename = "test"
+  // Stripping special characters
+  filename = encodeURIComponent(filename) + '.pdf'
+  // Setting response to 'attachment' (download).
+  // If you use 'inline' here it will automatically open the PDF
+  res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"')
+  res.setHeader('Content-type', 'application/pdf')
+  const content = "abcd content"
+
+  doc.moveTo(50,50)
+    .lineTo(800,50)
+    .stroke()
+
+
+  doc.text(content, 50, 50)
+  doc.pipe(res)
+  doc.end()
+})
 
 module.exports = router;
